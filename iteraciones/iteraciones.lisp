7d1e2274581f160fbf8c8b22b0a9ecd12b67a48b
cynnbuss <cynnbuss.cb@gmail.com>
2026-06-15 01:12:11 -0300

    carga rteq 4 y 5 en iteraciones.lisp

--- iteraciones/iteraciones.lisp
+++ iteraciones/iteraciones.lisp
@@ -39,7 +39,9 @@
 ;; REQUERIMIENTO 2: TEMPORIZADOR AUTOMÁTICO
 ;; ============================================================
 
-(defun ubicar-fase (resto)    ;; ciclo ajustado a 225 segundos para incluir las fases de intermitencia de 3 segundos cada una.
+ ;; ciclo ajustado a 225 segundos para incluir las fases de intermitencia de 3 segundos cada una.
+
+(defun ubicar-fase (resto)
   (cond
     ((< resto 90) 'en-rojo)
     ((< resto 93) 'amarillo-intermitente)
@@ -49,7 +51,9 @@
     ((< resto 225) 'amarillo-intermitente)
     (t 'fase-invalida)))
 
-(defun timer (tiempo-unix)  ;; (mod tiempo-unix 225) asegura que el ciclo se repita cada 225 segundos.
+;; (mod tiempo-unix 225) asegura que el ciclo se repita cada 225 segundos.
+
+(defun timer (tiempo-unix) 
   (if (integerp tiempo-unix)
       (ubicar-fase 
         (mod tiempo-unix 225))
@@ -57,7 +61,7 @@
 
 
 ;; ============================================================
-;; REQUERIMIENTO 3: SISTEMA DE AUDITORÍA. (Extensión 2 de la Iteración 2)
+;; REQUERIMIENTO 3: SISTEMA DE AUDITORÍA. (Extension 2 de la Iteracion 2)
 ;; ============================================================
 
 (defun es-color-valido (color)
@@ -67,19 +71,89 @@
    (equal color 'en-amarillo)
    (equal color 'amarillo-intermitente)))
 
-
+;; validacion de datos
 (defun informe (tiempo-epoch color-anterior color-nuevo)
-  (if (and (integerp tiempo-epoch) ;; validación de datos
+  (if (and (integerp tiempo-epoch)
            (es-color-valido color-anterior)
            (es-color-valido color-nuevo))
 
-      (with-open-file ;; gestiona la apertura y cierre automático
+      (with-open-file ;;sobre la aperturta y cierre autoimatico de nuestro archivo 
           (archivo "informe-ejecucion-semaforo.txt" 
                   :direction :output ;; define que vamos a escribir
                   :if-exists :append ;; evita sobrescribir
-                  :if-does-not-exist :create);; crea el archivo automáticamente si no existe
+                  :if-does-not-exist :create)
 
         (format archivo "Fecha ~a: cambio de ~a a ~a~%" tiempo-epoch color-anterior color-nuevo))
-  'error-datos-invalidos)) ;; si la validación falla, devuelve el error sin intentar acceder al disco
+  'error-datos-invalidos))
 
+;; ============================================================
+;; REQUERIMIENTO 4: ANALISIS DE CICLOS SEMAFORICOS
+;; ============================================================
+
+;; ============================================================
+;; FUNCIÓN 4-a: duracion-ciclo
+;; NATURALEZA: Pura
+;; ESTRATEGIA DE CONTROL: Condicional MUltiple
+;; IMPACTO EN MEMORIA: No destructiva
+;; ============================================================
+
+(defun duracion-ciclo (segundos)
+  (cond
+    ((or (not (numberp segundos))
+         (<= segundos 0))
+     "Error: La duracion ingresada debe ser un numero positivo")
+
+    ((and (>= segundos 35)
+          (<= segundos 150))
+     (list segundos 'Rango-Optimo))
+
+    ((< segundos 35)
+     (list segundos 'Duracion-baja))
+
+    (t
+     (list segundos 'Duracion-alta))))
+
+
+;; ============================================================
+;; FUNCION 4-b: recomendacion-ciclo
+;; NATURALEZA: Pura
+;; ESTRATEGIA DE CONTROL: Condicional Multiple
+;; IMPACTO EN MEMORIA: No destructiva
+;; ============================================================
+
+(defun recomendacion-ciclo (duracion)
+  (cond
+    ((or (not (numberp duracion))
+         (<= duracion 0))
+     "Error: La duracion ingresada debe ser un numero positivo")
+
+    ((and (>= duracion 35)
+          (<= duracion 150))
+     "Recomendacion: Ciclo optimo")
+
+    ((< duracion 35)
+     "Recomendacion: Aumentar tiempo")
+
+    (t
+     "Recomendacion: Reducir tiempo")))
+
+
+;; ============================================================
+;; REQUERIMIENTO 5: PLANIFICACION TEMPORAL
+;; ============================================================
+
+;; ============================================================
+;; FUNCIÓN: ciclos-por-tiempo
+;; NATURALEZA: Pura
+;; ESTRATEGIA DE CONTROL: Condicional Simple con TRUNCATE
+;; IMPACTO EN MEMORIA: No destructiva
+;; ============================================================
+
+(defun ciclos-por-tiempo (minutos)
+  (if (and (numberp minutos)
+           (>= minutos 0))
+      (truncate
+       (/ (* minutos 60)
+          225))
+      "Error: los minutos deben ser un numero positivo"))
 
