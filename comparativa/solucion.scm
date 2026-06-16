552bb1624a3c5205d69be560edfdfe26050ffe63
cynnbuss <cynnbuss.cb@gmail.com>
2026-06-15 21:41:47 -0300

    Update solucion.scm

--- comparativa/solucion.scm
+++ comparativa/solucion.scm
@@ -15,18 +15,24 @@
 ;; y la comunidad de software libre a través de GIMP 
 ;;(utiliza una variante llamada TinyScheme para sus plugins Script-Fu). 
 
+;; ============================================================
+;; FUNCIÓN: transicion
+;; NATURALEZA: Pura (Retorna un estado sin efectos secundarios)
+;; ESTRATEGIA: Condicional Simple (Adaptada de Common Lisp)
+;; IMPACTO EN MEMORIA: No destructiva
+;; ============================================================
 
 
 
 
 
 ;;;; ============================================================
-;;;; FASE 3: ESTUDIO COMPARATIVO - LISP-1 vs Lisp-2 y TCO
+;;;; FASE 3: ESTUDIO COMPARATIVO - LISP-1 (Scheme) vs Lisp-2 y TCO
 ;;;; ============================================================
 
 ;; 1. Lisp-1 vs Lisp-2: Espacios de Nombres
 ;; En Common Lisp (un "Lisp-2"), los símbolos tienen espacios de nombres 
-;; distintos para variables y funciones [2, 3]. Por esto, cuando pasamos 
+;; distintos para variables y funciones. Por esto, cuando pasamos 
 ;; una función como argumento, necesitamos el operador #' (abreviatura de 
 ;; function) para decirle al intérprete: "busca en el espacio de funciones", 
 ;; y luego usar funcall para ejecutarla.
@@ -41,13 +47,11 @@
 ;;;;;; ============================================================
 ;; 2. Estructuración para Optimización de Llamada de Cola (TCO)
 ;; Para asegurar que el compilador de Scheme no agote la pila de memoria 
-;; (Stack Overflow), se estructuró la recursividad de forma que la llamada 
-;; recursiva sea la última acción realizada por la función [4].
+;; (conocido como Stack Overflow), se estructuró la recursividad de forma que la llamada 
+;; recursiva sea la última acción realizada por la función.
 ;; 
 ;; En una función recursiva de cola (Tail Recursive), el resultado de la 
 ;; llamada recursiva se devuelve directamente sin realizar operaciones 
-;; pendientes (como sumas o construcciones de listas posteriores). Esto 
-;; permite que el sistema reutilice el marco de pila actual en lugar de 
+;; pendientes. Esto permite que el sistema reutilice el marco de pila actual en lugar de 
 ;; crear uno nuevo, garantizando una eficiencia de memoria equivalente a 
-;; un bucle imperativo, técnica fundamental dentro de la programación 
-;; funcional para el manejo de procesos repetitivos
+;; un bucle imperativo
