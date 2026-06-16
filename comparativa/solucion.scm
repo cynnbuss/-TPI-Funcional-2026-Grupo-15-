43e0d8ddf7dd8217f863810cb4f34c76193c9bbf
cynnbuss <cynnbuss.cb@gmail.com>
2026-06-15 21:35:34 -0300

    Update solucion.scm

--- comparativa/solucion.scm
+++ comparativa/solucion.scm
@@ -3,12 +3,14 @@
 ;;A diferencia de Common Lisp, que es un lenguaje extenso y multiparadigma 
 ;;Scheme se centra en la elegancia conceptual y la simplicidad sintáctica. 
 
-;;Industrias y Áreas de Uso: 
+;;Industrias y Áreas de Uso:
+
 ;;Se utiliza principalmente en el ámbito académico y de investigación 
 ;;(es el lenguaje base del famoso libro SICP), 
 ;;en sistemas embebidos, y como lenguaje de extensión o scripting para otras aplicaciones.
 
-;;Empresas conocidas: 
+;;Empresas conocidas:
+;; Google: Fue utilizado históricamente en el motor de App Inventor
 ;;Cisco (utiliza Scheme en procesos de control de calidad y herramientas internas), ;;Google (históricamente en App Inventor) 
 ;; y la comunidad de software libre a través de GIMP 
 ;;(utiliza una variante llamada TinyScheme para sus plugins Script-Fu). 
